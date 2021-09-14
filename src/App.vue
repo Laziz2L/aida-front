176427ed543ee466605b5d9737f64144da4e6f8c
Laziz <lazizabdullaev23@gmail.com>
2021-09-15 00:23:56 +0300

    add registration

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <category :background="'./assets/Photo.png'" :title="'Посмотреть на цветочки'" :subtitle="'27 локаций'" />
+  <!-- <category :background="'./assets/Photo.png'" :title="'Посмотреть на цветочки'" :subtitle="'27 локаций'" /> -->
+  <registration />
 </template>
 
 <script>
-import EventCategoryCard from './components/EventCategoryCard.vue';
+// import EventCategoryCard from './components/EventCategoryCard.vue';
+import Registration from './components/Registration.vue';
 
 export default {
   name: 'App',
   components: {
-    'category' : EventCategoryCard
+    // 'category' : EventCategoryCard,
+    Registration
   }
 }
 </script>
---
+++ src/components/Registration.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="registration">
+            <img src="../assets/avatar.png" class="avatar" />
+            <form class="registration-form">
+                <input
+                    type="text"
+                    class="registration-form__input"
+                    placeholder="Введите имя"
+                />
+                <input
+                    type="text"
+                    class="registration-form__input"
+                    placeholder="Сколько вам лет?"
+                />
+                <div class="registration-form__sex-choose">
+                    <button type="button" class="btn registration-form__sex-btn ">
+                        Мужчина
+                    </button>
+                    <button type="button" class="btn registration-form__sex-btn">
+                        Женщина
+                    </button>
+                </div>
+            </form>
+
+            <br>
+            <br>
+            <br>
+            <br>
+
+            <button class="btn registration__save-btn">Сохранить</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
