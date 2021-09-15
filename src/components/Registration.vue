8cf8c8c52be38ddb9a0397d32a3a525a93ad623c
Laziz <lazizabdullaev23@gmail.com>
2021-09-16 00:01:48 +0300

    add registration vertical align

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,40 +1,91 @@
 <template>
     <div class="container">
         <div class="registration">
-            <img src="../assets/avatar.png" class="avatar" />
-            <form class="registration-form">
-                <input
-                    type="text"
-                    class="registration-form__input"
-                    placeholder="Введите имя"
-                />
-                <input
-                    type="text"
-                    class="registration-form__input"
-                    placeholder="Сколько вам лет?"
-                />
-                <div class="registration-form__sex-choose">
-                    <button type="button" class="btn registration-form__sex-btn ">
-                        Мужчина
-                    </button>
-                    <button type="button" class="btn registration-form__sex-btn">
-                        Женщина
-                    </button>
-                </div>
-            </form>
+            <div id="vertical-center" class="registration-center">
+                <img src="../assets/avatar.png" class="avatar" />
+                <form class="registration-form">
+                    <input
+                        v-model="name"
+                        type="text"
+                        class="registration-form__input"
+                        placeholder="Введите имя"
+                    />
+                    <input
+                        v-model="age"
+                        type="text"
+                        class="registration-form__input"
+                        placeholder="Сколько вам лет?"
+                    />
+                    <div class="registration-form__sex-choose">
+                        <button
+                            @click="setMale()"
+                            type="button"
+                            class="btn registration-form__sex-btn"
+                            v-bind:class="{ active: male }"
+                        >
+                            Мужчина
+                        </button>
+                        <button
+                            @click="setFemale()"
+                            type="button"
+                            class="btn registration-form__sex-btn"
+                            v-bind:class="{ active: female }"
+                        >
+                            Женщина
+                        </button>
+                    </div>
+                </form>
+            </div>
 
-            <br>
-            <br>
-            <br>
-            <br>
-
-            <button class="btn registration__save-btn">Сохранить</button>
+            <button id="save-btn" class="btn registration__save-btn">
+                Сохранить
+            </button>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            name: "",
+            age: "",
+            male: false,
+            female: false,
+        };
+    },
+    methods: {
+        setMale() {
+            if (this.female) this.female = false;
+            this.male = !this.male;
+        },
+        setFemale() {
+            if (this.male) this.male = false;
+            this.female = !this.female;
+        },
+    },
+    mounted() {
+        let screenHeight =
+            window.innerHeight ||
+            document.documentElement.clientHeight ||
+            document.body.clientHeight;
+        let saveBtn = document.getElementById("save-btn");
+        let saveBtnHeight = saveBtn.offsetHeight;
+        let center = document.getElementById("vertical-center");
+        // let centerHeight = center.offsetHeight;
+        let centerHeight = 350;
+        console.log([centerHeight, saveBtnHeight, screenHeight]);
+        if (screenHeight > centerHeight + saveBtnHeight + 20) {
+            center.style.marginTop = (screenHeight - centerHeight) / 2 + "px";
+            saveBtn.style.marginTop =
+                screenHeight -
+                (screenHeight - centerHeight) / 2 -
+                centerHeight -
+                (saveBtnHeight + 20) +
+                "px";
+        }
+    },
+};
 </script>
 
 <style>
